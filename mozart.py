e2809c881db430601f8e87e1c86c4b51b62965d3
Hithesh <hithesh@localhost.localdomain>
2021-10-11 20:30:08 +0530

    the core of mozart is done and is working perfectly with Spotify Bot

--- mozart.py
+++ mozart.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from discord.ext import commands
+import discord
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -9,22 +9,25 @@ from spotipy.oauth2 import SpotifyOAuth
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix = '!')
+client = discord.Client()
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth( client_id = os.getenv('CLIENT_ID'),
                                                client_secret = os.getenv('CLIENT_SECRET'),
                                                redirect_uri = os.getenv('REDIRECT_URI'),
                                                 scope = "playlist-modify-public"))
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
 
-@bot.command(name = 'add')
-async def add(ctx, song_id):
-    tracks = [song_id]
-    sp.playlist_add_items("5FZviWMbPvn9o5ldwRKhf0", tracks)
-    song_name = sp.track(song_id)['name']
-    await ctx.send(f'Added "{song_name}" successfully')
+@client.event
+async def on_message(message):
+    msg = message.content.split(" ")
+    if len(msg) == 2 and msg[0] == '!add':
+        song_id = msg[1]
+        tracks = [song_id]
+        sp.playlist_add_items("5FZviWMbPvn9o5ldwRKhf0", tracks)
+        song_name = sp.track(song_id)['name']
+        await message.channel.send(f'Added "{song_name}" successfully!')
 
-bot.run(TOKEN)
+client.run(TOKEN)
